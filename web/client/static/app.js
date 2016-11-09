cf3559c78c900a709abc20d0671a5830de3dc843
Chris Bautista <c.bautista.626@gmail.com>
2016-11-08 18:03:36 -0800

    added a bunch of changes

--- web/client/static/app.js
+++ web/client/static/app.js
@@ -20,18 +20,43 @@ app.config(function($routeProvider, $httpProvider) {
   );
 */
   $routeProvider
+  /* Root */
   .when('/', {
     templateUrl: 'partials/main.html',
     controller: 'mainController'
   })
-  .when('/cbaut', {
-    templateUrl: 'partials/cbaut.html',
-    controller: 'cbautController'
-  })
+  /* Nav */
   .when('/schedule', {
     templateUrl: 'partials/schedule.html',
     controller: 'scheduleController'
   })
+  .when('/bookClub', {
+    templateUrl: 'partials/bookClub.html'
+  })
+  .when('/games', {
+    templateUrl: 'partials/games.html'
+  })
+  .when('/ninjas', {
+    templateUrl: 'partials/ninjas.html'
+  })
+  .when('/pingPong', {
+    templateUrl: 'partials/pingPong.html'
+  })
+  .when('/timeline', {
+    templateUrl: 'partials/timeline.html'
+  })
+  .when('/projects', {
+    templateUrl: 'partials/projects.html'
+  })
+  .when('/about', {
+    templateUrl: 'partials/about.html'
+  })
+  /* Ninjas */
+  .when('/cbaut', {
+    templateUrl: 'partials/ninjas/cbaut.html',
+    controller: 'cbautController'
+  })
+  /* If none of these, go back to root */
   .otherwise({
     redirectTo: '/'
   });
